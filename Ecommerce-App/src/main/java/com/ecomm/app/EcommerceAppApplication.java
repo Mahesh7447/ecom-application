6ca72cf80e25645aa774f4252129a077605fd298
Mahesh Popale <popalemahesh887@gmail.com>
2024-07-02 14:56:01 +0530

    project creation completed

---
+++ Ecommerce-App/src/main/java/com/ecomm/app/EcommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAppApplication.class, args);
+	}
+
+}
